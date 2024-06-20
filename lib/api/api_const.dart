43131e425a262f0409f24cc511752643fa13baa4
akshaybaraiya5 <139554275+akshaybaraiya5@users.noreply.github.com>
2024-06-20 16:44:52 +0530

    Update api_const.dart

--- lib/api/api_const.dart
+++ lib/api/api_const.dart
@@ -6,7 +6,7 @@ class APIConst {
 
   static const swapBaseUrl = 'https://developer.remaker.ai/api/remaker/v1/face-swap/';
   static const createJobUrl = '${swapBaseUrl}create-job';
-  static const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzcxOTEyOCwicHJvZHVjdF9jb2RlIjoiMDY3MDAzIiwidGltZSI6MTcxMTA4Mjg5OH0.Jf4MvvmnIv_7OFlVFbAD1oxGxVxnG7saUCJUsDq6330';
+  static const apiKey = '';
 
 
 
